fae1b27c8a63ff0f6715d8d4ceb2ab422a399404
SeaBassy4 <sebastianperezgonzalez4@gmail.com>
2026-01-29 15:29:13 -0700

    fix

---
+++ app/src/main/java/com/example/appcomprayventa/OpcionesLogin.kt
@@ -0,0 +1,27 @@
+package com.example.appcomprayventa
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.appcomprayventa.Opciones_Login.Login_email
+import com.example.appcomprayventa.databinding.ActivityOpcionesLoginBinding
+
+class OpcionesLogin : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOpcionesLoginBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityOpcionesLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.IngresarEmail.setOnClickListener {
+            startActivity(Intent(this@OpcionesLogin, Login_email::class.java))
+        }
+
+    }
+}
