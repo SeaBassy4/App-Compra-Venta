5942577bebb833b394d004b1b5e75e79cc715ddd
SeaBassy4 <sebastianperezgonzalez4@gmail.com>
2026-02-11 10:21:37 -0700

    10 de febrero

--- app/src/main/java/com/example/appcomprayventa/MainActivity.kt
+++ app/src/main/java/com/example/appcomprayventa/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.appcomprayventa
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.appcomprayventa.Fragmentos.FragmentChats
@@ -7,15 +8,21 @@ import com.example.appcomprayventa.Fragmentos.FragmentCuenta
 import com.example.appcomprayventa.Fragmentos.FragmentInicio
 import com.example.appcomprayventa.Fragmentos.FragmentMisAnuncios
 import com.example.appcomprayventa.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        comprobarSesion()
         verFragmentInicio()
         binding.BottomNV.setOnItemSelectedListener { item ->
             when(item.itemId){
@@ -42,6 +49,13 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+    }
+
+    private fun comprobarSesion(){
+        if(firebaseAuth.currentUser == null){
+            startActivity(Intent(this, OpcionesLogin::class.java))
+            finishAffinity()
+        }
     }
 
     private fun verFragmentInicio() {
--- app/src/main/java/com/example/appcomprayventa/Registro_email.kt
+++ app/src/main/java/com/example/appcomprayventa/Registro_email.kt
@@ -3,12 +3,15 @@ package com.example.appcomprayventa
 import android.app.ProgressDialog
 import android.os.Bundle
 import android.content.Intent
+import android.util.Patterns
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.appcomprayventa.databinding.ActivityRegistroEmailBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class Registro_email : AppCompatActivity() {
 
@@ -42,5 +45,84 @@ class Registro_email : AppCompatActivity() {
         email = binding.EtEmail.text.toString().trim()
         password = binding.EtPassword.text.toString().trim()
         r_password = binding.EtRPassword.text.toString().trim()
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            binding.EtEmail.error = "Email invalido"
+            binding.EtEmail.requestFocus()
+        }
+        else if (email.isEmpty()){
+            binding.EtEmail.error = "Ingrese un email"
+            binding.EtEmail.requestFocus()
+        }
+        else if(password.isEmpty()){
+            binding.EtPassword.error = "Ingrese el Password"
+            binding.EtPassword.requestFocus()
+        }
+        else if(r_password.isEmpty()){
+            binding.EtRPassword.error = "Repita el password"
+            binding.EtRPassword.requestFocus()
+        }
+        else if (password != r_password){
+            binding.EtRPassword.error = "No coinciden"
+            binding.EtRPassword.requestFocus()
+        }
+        else {
+            registrarUsuario()
+        }
     }
+
+    private fun registrarUsuario() {
+        progressDialog.setMessage("Creando cuenta")
+        progressDialog.show()
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+            llenarInfoBD()
+        }
+            .addOnFailureListener { exception ->
+                progressDialog.dismiss()
+                Toast.makeText(this,
+                    "No se registro el usuario debido a ${exception.message}",
+                    Toast.LENGTH_SHORT).show()
+            }
+
+    }
+
+    private fun llenarInfoBD() {
+        progressDialog.setMessage("Guardando Informacion")
+
+        val tiempo = Constantes.obtenerTiempoDis()
+        val emailUsuario = firebaseAuth.currentUser!!.email
+        val uidUsuario = firebaseAuth.uid
+
+        val hashMap = HashMap<String, Any>()
+        hashMap["nombres"] = ""
+        hashMap["codigoTelefono"] = ""
+        hashMap["telefono"] = ""
+        hashMap["urlImagenPerfil"] = ""
+        hashMap["proveedor"] = "Email"
+        hashMap["escribiendo"] = ""
+        hashMap["tiempo"] = tiempo
+        hashMap["online"] = true
+        hashMap["email"] = "${emailUsuario}"
+        hashMap["uid"] = "${uidUsuario}"
+        hashMap["fecha_nac"] = ""
+
+        val ref = FirebaseDatabase.getInstance().getReference("Usuarios")
+        ref.child(uidUsuario!!)
+            .setValue(hashMap)
+            .addOnSuccessListener {
+                progressDialog.dismiss()
+                startActivity(Intent(this, MainActivity::class.java))
+                finishAffinity()
+            }
+            .addOnFailureListener {
+                progressDialog.dismiss()
+                Toast.makeText(this, "No se registro debido a ${exception.message}",
+                    Toast.LENGTH_SHORT)
+                    .show()
+            }
+    }
+
+
 }
